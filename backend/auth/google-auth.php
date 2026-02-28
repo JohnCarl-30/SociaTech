72b7f997f689219ba8886e8200b1c0ae75b6a46b
JohnCarl-30 <johncarlsantos30@gmail.com>
2026-02-28 14:17:34 +0800

    Update google-auth.php

--- backend/auth/google-auth.php
+++ backend/auth/google-auth.php
@@ -1,12 +1,5 @@
 <?php
 
-
-
-header("Access-Control-Allow-Origin: https://socia-tech.vercel.app");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
